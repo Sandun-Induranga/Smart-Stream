08fd78af0912bd15b68166dd69142629e528dd50
Sandun Induranga <sandutharu40@gmail.com>
2024-01-14 19:22:38 +0530

    Added Homepage and Constants

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const routes = {
+  SIGN_IN: "/",
+  HOME: "/home",
+};
---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Home = () => {
+  return <Box>Home</Box>;
+};
+
+export default Home;
