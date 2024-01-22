941daa5a54c4c75ff8bd4e68884ab6cfcb31beb7
Sandun Induranga <sandutharu40@gmail.com>
2024-01-22 23:29:58 +0530

    Content Updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Home } from "@mui/icons-material";
+import Home from "./containers/Home/Home";
 
 const App = () => {
   return (
