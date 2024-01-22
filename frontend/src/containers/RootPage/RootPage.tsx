8373504afc8770a0133a65ae6d0de8436938c363
Sandun Induranga <sandutharu40@gmail.com>
2024-01-23 01:07:57 +0530

    Content Updated

--- frontend/src/containers/RootPage/RootPage.tsx
+++ frontend/src/containers/RootPage/RootPage.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  HOME_ROUTE,
+  PREDICT_ROUTE,
+  STUDENT_ROUTE,
+} from "../../constants/routes";
+import { Home } from "@mui/icons-material";
+import Student from "../Student/Student";
+import Prediction from "../Prediction/Prediction";
 
 const RootPage = () => {
-  return <div>RootPage</div>;
+  return (
+    <Routes>
+      <Route path={HOME_ROUTE} element={<Home />} />
+      <Route path={STUDENT_ROUTE} element={<Student />} />
+      <Route path={PREDICT_ROUTE} element={<Prediction />} />
+    </Routes>
+  );
 };
 
 export default RootPage;
