194d3f6b15a754f29a6b3481ec2bea52bc7d544c
Sandun Induranga <sandutharu40@gmail.com>
2024-01-22 23:42:25 +0530

    Content Updated

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -1,10 +1,58 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import Footer from "../../components/Footer";
+import studentsImage from "../../assets/students.jpg";
+import predictionImage from "../../assets/prediction.jpg";
 
 const Home = () => {
   return (
     <Box>
       <Typography variant="h4">Welcome to Smart Stream</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 20,
+          p: 10,
+        }}
+      >
+        <Box
+          component={Paper}
+          sx={{ p: 2, borderRadius: 4, cursor: "pointer" }}
+        >
+          <Box
+            component={"img"}
+            src={studentsImage}
+            sx={{ width: "40vw", height: "50vh", borderRadius: 8 }}
+          />
+          <Typography
+            variant="h6"
+            mt={4}
+            color={"GrayText"}
+            textAlign={"center"}
+          >
+            Manage Students
+          </Typography>
+        </Box>
+        <Box
+          component={Paper}
+          sx={{ p: 2, borderRadius: 4, cursor: "pointer" }}
+        >
+          <Box
+            component={"img"}
+            src={predictionImage}
+            sx={{ width: "40vw", height: "50vh", borderRadius: 8 }}
+          />
+          <Typography
+            variant="h6"
+            mt={4}
+            color={"GrayText"}
+            textAlign={"center"}
+          >
+            Predict Streams
+          </Typography>
+        </Box>
+      </Box>
       <Footer />
     </Box>
   );
