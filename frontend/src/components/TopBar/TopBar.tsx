2ee0585d3205291d519413f3d1af4a4d073b7011
Sandun Induranga <sandutharu40@gmail.com>
2024-02-05 00:35:36 +0530

    Content Updated

--- frontend/src/components/TopBar/TopBar.tsx
+++ frontend/src/components/TopBar/TopBar.tsx
@@ -8,7 +8,6 @@ import {
 import { Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { HOME_ROUTE, SIGNIN_ROUTE } from "../../constants/routes";
-import logo from "../../assets/logo.png";
 
 const TopBar = ({ isHome = false }) => {
   const navigate = useNavigate();
@@ -24,23 +23,25 @@ const TopBar = ({ isHome = false }) => {
         px: 4,
       }}
     >
-      {!isHome && (
-        <Box
-          sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
-          onClick={() => navigate(HOME_ROUTE)}
+      <Box>
+        <Typography
+          variant="h6"
+          sx={{ color: "white", fontWeight: 1000, fontStyle: "italic" }}
         >
-          <ArrowBackIos sx={{ color: "white", pl: 2 }} />
-          <Typography variant="body1" sx={{ color: "white" }}>
-            Back
-          </Typography>
-        </Box>
-      )}
-      <Typography
-        variant="h6"
-        sx={{ color: "white", fontWeight: 1000, fontStyle: "italic" }}
-      >
-        Smart Stream
-      </Typography>
+          Smart Stream
+        </Typography>
+        {!isHome && (
+          <Box
+            sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+            onClick={() => navigate(HOME_ROUTE)}
+          >
+            <ArrowBackIos sx={{ color: "white" }} />
+            <Typography variant="body1" sx={{ color: "white" }}>
+              Back
+            </Typography>
+          </Box>
+        )}
+      </Box>
       <Box>
         <VerifiedUser sx={{ color: "white", pr: 2 }} />
         <NotificationsActive sx={{ color: "white", pr: 2 }} />
