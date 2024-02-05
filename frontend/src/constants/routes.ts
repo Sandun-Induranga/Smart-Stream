a8e8ce6d934d345081eb11a7de45177e5065c5d1
Sandun Induranga <sandutharu40@gmail.com>
2024-02-05 11:00:55 +0530

    Updated

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,7 +1,7 @@
 export const HOME_ROUTE =
-  "https://sandun-induranga.github.io/Smart-Stream//home";
-export const SIGNIN_ROUTE = "https://sandun-induranga.github.io/Smart-Stream//";
+  "https://sandun-induranga.github.io/Smart-Stream/home";
+export const SIGNIN_ROUTE = "https://sandun-induranga.github.io/Smart-Stream/";
 export const STUDENT_ROUTE =
-  "https://sandun-induranga.github.io/Smart-Stream//student";
+  "https://sandun-induranga.github.io/Smart-Stream/student";
 export const PREDICT_ROUTE =
-  "https://sandun-induranga.github.io/Smart-Stream//predict";
+  "https://sandun-induranga.github.io/Smart-Stream/predict";
