fb7dba86ab7e5024732421915848dcbe075bc638
Sandun Induranga <sandutharu40@gmail.com>
2024-02-05 10:50:09 +0530

    Content Updated

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,4 +1,7 @@
-export const HOME_ROUTE = "/home";
-export const SIGNIN_ROUTE = "/";
-export const STUDENT_ROUTE = "/student";
-export const PREDICT_ROUTE = "/predict";
+export const HOME_ROUTE =
+  "https://sandun-induranga.github.io/Smart-Stream//home";
+export const SIGNIN_ROUTE = "https://sandun-induranga.github.io/Smart-Stream//";
+export const STUDENT_ROUTE =
+  "https://sandun-induranga.github.io/Smart-Stream//student";
+export const PREDICT_ROUTE =
+  "https://sandun-induranga.github.io/Smart-Stream//predict";
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
