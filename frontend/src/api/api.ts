83a103a449b140d71b8a59af026c7417a19ad16d
Sandun Induranga <sandutharu40@gmail.com>
2024-02-08 15:06:45 +0530

    Updated

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://ec2-44-212-4-241.compute-1.amazonaws.com:8000/",
+  baseURL: "https://ec2-44-212-4-241.compute-1.amazonaws.com:8000/",
 });
