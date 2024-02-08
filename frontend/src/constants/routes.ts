be47fa579f5afbf3253732eaf2d302de5114da10
Sandun Induranga <sandutharu40@gmail.com>
2024-02-08 15:02:58 +0530

    Content Updated

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -2,3 +2,4 @@ export const HOME_ROUTE = "/home";
 export const SIGNIN_ROUTE = "/";
 export const STUDENT_ROUTE = "/student";
 export const PREDICT_ROUTE = "/predict";
+// https://smart-stream-rgu3mizyk-sandun-indurangas-projects.vercel.app/#/home
